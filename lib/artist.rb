aa3826a70b79b4e70a377e42e0cdebf44eaef816
dianakw8591 <dianakw@gmail.com>
2020-01-30 10:15:21 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Artist
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def songs
+        Song.all.select  {|song| song.artist == self}
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        song = Song.new(song_name)
+        add_song(song)
+    end
+
+    def self.song_count
+        self.all.sum {|artist| artist.songs.count}
+    end
+end
