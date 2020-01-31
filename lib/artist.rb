f421c920ab3a2c55b5587cd096999f7c31f02f5d
javila35 <josepharthuravila@gmail.com>
2020-01-31 14:29:04 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def songs
+        Song.all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song)
+        new_song = Song.new(song)
+        new_song.artist = self
+    end
+
+    def self.song_count
+        Song.all.size
+    end
+end
