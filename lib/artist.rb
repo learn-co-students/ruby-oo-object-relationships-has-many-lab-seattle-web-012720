19c74bb7a2175a7f3454618ac2789c94da3f3dc7
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-05-06 14:49:18 -0700

    going over oop

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,14 @@
 class Artist
 
-    attr_accessor :name, :songs
+    attr_accessor :name
 
     def initialize(name)
         @name = name
+        @songs = []
     end
 
-    def songs
-        Song.all.select do |song|
-         song.artist == self
-        end
+    def my_songs
+        Songs.all.select { |song| song.name == self }
     end
-
-    def add_song(song)
-       song.artist = self
-    end
-
-    def add_song_by_name(song_name)
-        singer = Song.new(song_name)
-        add_song(singer)
-    end
-
-    def self.song_count
-        Song.all.count
-    end
-
-
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
-
-    attr_accessor :artist, :name
+    attr_accessor :name, :genre, :artist
 
     @@all = []
 
-    def initialize(name)
+    def initialize(name, genre)
         @name = name
+        @genre = genre
         @@all << self
     end
 
@@ -13,13 +13,32 @@ class Song
         @@all
     end
 
-    def artist_name
-        if self.artist
-            self.artist.name
-        else
-            nil
-        end
-    end
 end
+
+
+class Artist
+
+    attr_accessor :name, :songs
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def add_song_by_name(name, genre)
+        song = Song.new(name, genre)
+        song.artist = self
+    end
+
+    def find_my_songs
+        Songs.all.select { |song| song.artist == self }
+    end
+
+end
+
+
+artist1 = Artist.new("first_artist")
+artist1.add_song_by_name("first_song", "first_genre")
+puts artist1.find_my_songs
 
 
